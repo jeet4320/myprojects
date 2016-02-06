34e39bc1c5171a0139565f1f25798b6054182a8b
jeet4320 <jeetenpatil@gmail.com>
2016-02-06 17:02:34 -0600

    make it better

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ http.createServer(function (req, res) {
     util.log(util.inspect(req)) // this line helps you inspect the request so you can see whether the data is in the url (GET) or the req body (POST)
     util.log('Request recieved: \nmethod: ' + req.method + '\nurl: ' + req.url) // this line logs just the method and url
 
+    var returnObject = {message: "Hello World!"};
+    var returnObjectString = JSON.stringify(returnObject);
+    
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     req.on('data', function (chunk) {
         console.log('GOT DATA!');
     });
-    res.end('callback(\'{\"msg\": \"OK\"}\')');
+    res.end(returnObjectString);
 
 }).listen(8200);
 console.log('Server running on port 8200');
