ebb38e045b9ce5f073edbaaec5c8fc0cf58f053f
jeet4320 <jeetenpatil@gmail.com>
2016-02-06 19:23:59 -0600

    make it better

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var express = require('express');
+var app = express();
 
-var app = express.createServer();
-app.use(express.bodyParser());
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
 
 app.post('/search', function(req, res){
    
