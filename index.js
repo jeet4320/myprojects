ed4d1c2a35d04b82746c79cda127e5789b620487
jeet4320 <jeetenpatil@gmail.com>
2016-02-06 20:58:04 -0600

    make it better

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app            =         express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -22,8 +22,8 @@ app.post('/login',function(req,res){
   console.log("User name = "+user_name+", password is "+password);
   res.end("Done");
 });
-app.listen(3000,function(){
-  console.log("Started on PORT 3000");
-})
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
