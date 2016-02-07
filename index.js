86bde45526f6b11e2141dd0b6f6a0df1491ad20b
jeet4320 <jeetenpatil@gmail.com>
2016-02-06 19:47:33 -0600

    make it better

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 var http = require('http');
 var util = require('util');
-var express = require('express');
-var app = express();
-app.set('port', (process.env.PORT || 8200));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
 http.createServer(function (req, res) {
 
 switch(req.url) {
